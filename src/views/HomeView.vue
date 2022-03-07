6b6b4d3017edb82d94568d22d112f1ba59629259
Fakhruddin Noor <fakhruddin.noor1337@gmail.com>
2022-03-07 14:26:26 +0700

    convert index.html and product.html to vue

---
+++ src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroShayna />
+    <WomanShayna />
+    <InstaShayna />
+    <PartnerShayna />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import FooterShayna from '@/components/FooterShayna.vue';
+import HeaderShayna from '@/components/HeaderShayna.vue';
+import HeroShayna from '@/components/HeroShayna.vue';
+import InstaShayna from '@/components/InstaShayna.vue';
+import PartnerShayna from '@/components/PartnerShayna.vue';
+import WomanShayna from '@/components/WomanShayna.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    FooterShayna,
+    HeaderShayna,
+    HeroShayna,
+    InstaShayna,
+    PartnerShayna,
+    WomanShayna,
+  }
+}
+</script>
